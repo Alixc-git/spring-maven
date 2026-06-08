a2773789e4f0bc2d9f377befc58994d3198924d5
Alixc-git <lexseck@gmail.com>
2026-06-08 16:48:08 +0000

    Create DockerDemoApplication.java

---
+++ src/main/java/com/example/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,17 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DockerDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+}
+
+@RestController
+class HelloWorldController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
